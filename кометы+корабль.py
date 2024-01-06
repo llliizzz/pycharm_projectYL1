10619ac44d37b8e3c35026d79b874edf23028729
alina <Akhmedzhanova.A@yandex.ru>
2024-01-06 18:36:58 +0400

    анимация монетки

--- кометы+корабль.py
+++ кометы+корабль.py
@@ -1,17 +1,16 @@
 import os
 import sys
+from random import randint
 
 import pygame
+
 from ball import Ball
-# from coin2 import Coin
-from coin import AnimatedCoin
-from random import randint
+from coin2 import Coin
+from ship1 import SpaceShip
 
 pygame.init()
 pygame.time.set_timer(pygame.USEREVENT, 1000)
-
 BLACK = (0, 0, 0)
-
 W, H = 1000, 570
 sc = pygame.display.set_mode((W, H))
 
@@ -33,14 +32,16 @@ def load_image(name, colorkey=None):
 
 bg = load_image("background.jpg")
 
+# ship = pygame.image.load('data/ship.png').convert_alpha()
+# t_rect = ship.get_rect(centerx=W // 2, bottom=H - 20)
+# mask = pygame.mask.from_surface(ship)
+ship = SpaceShip("data\ship.png", W, H)
+
 clock = pygame.time.Clock()
 fps = 60
-
 balls_images = ['stone4.png', 'stone5.png', 'stone6.png']
 balls_surf = [load_image(path) for path in balls_images]
-
-coins_surf = load_image('coins.png')
-
+coins_surf = load_image('coin.png')
 balls = pygame.sprite.Group()
 coins = pygame.sprite.Group()
 
@@ -49,7 +50,6 @@ def createBall(group):
     indx = randint(0, len(balls_surf) - 1)
     x = randint(20, W - 20)
     speed = randint(1, 4)
-
     return Ball(x, speed, balls_surf[indx], group)
 
 
@@ -59,16 +59,34 @@ createBall(balls)
 def createCoin(group):
     # indx = randint(0, len(balls_surf) - 1)
     x = randint(20, W - 20)
-    y = 0
     speed = randint(1, 4)
-
-    return AnimatedCoin(load_image('coins.png'), 4, 2, x, y, group)
+    return Coin(x, speed, coins_surf, group)
 
 
 createCoin(coins)
 
-COIN = createCoin(coins)
+
+def collideBalls():
+    global game_score
+    for ball in balls:
+        # if t_rect.collidepoint(ball.rect.center):
+        # game_score += ball.score
+        if ship.t_rect.collidepoint(ball.rect.center):
+            # game_score += ball.score
+            ball.kill()
+
+
+def collideCoins():
+    global game_score
+    for coin in coins:
+        # if t_rect.collidepoint(coin.rect.center):
+        if ship.t_rect.collidepoint(coin.rect.center):
+            # game_score += ball.score
+            coin.kill()
+
+
 k = 0
+speed = 10
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -79,29 +97,24 @@ while True:
             if not (k % 5):
                 createCoin(coins)
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        ship.t_rect.x -= speed
+        if ship.t_rect.x < 0:
+            ship.t_rect.x = W - ship.t_rect.width
+    elif keys[pygame.K_RIGHT]:
+        ship.t_rect.x += speed
+        if ship.t_rect.x > W - ship.t_rect.width:
+            ship.t_rect.x = 0
+
     sc.blit(bg, (0, 0))
     balls.draw(sc)
-    COIN.draw(sc)
+    coins.draw(sc)
+    sc.blit(ship.image, ship.t_rect)
     pygame.display.update()
 
     clock.tick(fps)
-
     balls.update(H)
     coins.update(H)
-# if __name__ == '__main__':
-#     running = True
-#     clock = pygame.time.Clock()
-#     screen.fill((255, 255, 255))
-#
-#     all_sprites = pygame.sprite.Group()
-#
-#     drag = AnimatedCoin(load_image("pygame-8-1.png", -1), 8, 2,
-#                         width - 150, height - 150,
-#                         dragon, all_sprites)
-#
-#     while running:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 running = False
-#             elif event.type == pygame.USEREVENT:
-#                 createBall(balls)
+    collideBalls()
+    collideCoins()
