62c143cf6e299edaf1d4e1592711b62b4a5c711f
liza <sotnikova2308@yandex.ru>
2024-01-06 12:32:09 +0300

    +корабль (пока без пересечения по маске)

--- stones2.py
+++ stones2.py
@@ -1,10 +1,11 @@
 import os
 import sys
+from random import randint
 
 import pygame
+
 from ball import Ball
 from coin2 import Coin
-from random import randint
 
 pygame.init()
 pygame.time.set_timer(pygame.USEREVENT, 1000)
@@ -31,6 +32,10 @@ def load_image(name, colorkey=None):
 
 
 bg = load_image("background.jpg")
+
+ship = pygame.image.load('data/ship.png').convert_alpha()
+t_rect = ship.get_rect(centerx=W // 2, bottom=H - 20)
+#mask = pygame.mask.from_surface(ship)
 
 clock = pygame.time.Clock()
 fps = 60
@@ -65,7 +70,25 @@ def createCoin(group):
 
 createCoin(coins)
 
+
+def collideBalls():
+    global game_score
+    for ball in balls:
+        if t_rect.collidepoint(ball.rect.center):
+            #game_score += ball.score
+            ball.kill()
+
+
+def collideCoins():
+    global game_score
+    for coin in coins:
+        if t_rect.collidepoint(coin.rect.center):
+            # game_score += ball.score
+            coin.kill()
+
+
 k = 0
+speed = 10
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -76,15 +99,29 @@ while True:
             if not (k % 5):
                 createCoin(coins)
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        t_rect.x -= speed
+        if t_rect.x < 0:
+            t_rect.x = W - t_rect.width
+    elif keys[pygame.K_RIGHT]:
+        t_rect.x += speed
+        if t_rect.x > W - t_rect.width:
+            t_rect.x = 0
+
     sc.blit(bg, (0, 0))
     balls.draw(sc)
     coins.draw(sc)
+    sc.blit(ship, t_rect)
     pygame.display.update()
 
     clock.tick(fps)
 
     balls.update(H)
     coins.update(H)
+
+    collideBalls()
+    collideCoins()
 # if __name__ == '__main__':
 #     running = True
 #     clock = pygame.time.Clock()
