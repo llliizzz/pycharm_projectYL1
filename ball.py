23afbfc65eeccd01cb7ccb1699785e0a6a31999b
liza <sotnikova2308@yandex.ru>
2024-01-07 10:14:43 +0300

    + маска в классах

--- ball.py
+++ ball.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ball(pygame.sprite.Sprite):
     def __init__(self, x, speed, surf, group):
         pygame.sprite.Sprite.__init__(self)
@@ -7,6 +8,7 @@ class Ball(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(x, 0))
         self.speed = speed
         self.add(group)
+        self.mask = pygame.mask.from_surface(self.image)
 
     def update(self, *args):
         if self.rect.y < args[0] - 20:
--- coin.py
+++ coin.py
@@ -9,6 +9,8 @@ class AnimatedCoin(pygame.sprite.Sprite):
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.rect = self.rect.move(x, y)
+        self.mask = pygame.mask.from_surface(self.image)
+
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
                                 sheet.get_height() // rows)
@@ -17,6 +19,7 @@ class AnimatedCoin(pygame.sprite.Sprite):
                 frame_location = (self.rect.w * i, self.rect.h * j)
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
+
     def update(self):
         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
         self.image = self.frames[self.cur_frame]
