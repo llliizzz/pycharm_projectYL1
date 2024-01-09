8eab22907ce191e89810813fb77ba3599230b462
alina <akhmedzhanova.a@yandex.ru>
2024-01-09 18:18:25 +0400

    для анимации

--- Heart.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Coin(pygame.sprite.Sprite):
-    def __init__(self, x,group):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = surf
-        # self.rect = self.image.get_rect(center=(x, 0))
-        # self.speed = speed
-        self.add(group)
