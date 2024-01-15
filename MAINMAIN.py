c91444bdda9606d109e7a0b3cb84e81e7ec19e9e
alina <Akhmedzhanova.A@yandex.ru>
2024-01-15 20:15:13 +0400

    все окошки соединены

---
+++ MAINMAIN.py
@@ -0,0 +1,245 @@
+import os
+import random
+import sqlite3
+import sys
+from random import randint
+import pygame
+from ball import Ball
+from coin2 import Coin
+from ship1 import SpaceShip
+from AnimatedLife import AnimatedLife
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    # else:
+    #     image = image.convert_alpha()
+    return image
+
+
+def pause(sc, W, H):
+    font = pygame.font.SysFont(None, 40)
+    WHITE = (255, 255, 255)
+    paused_text = font.render('Paused. Press SPACE to resume.', True, WHITE)
+    paused_rect = paused_text.get_rect(center=(W // 2, H // 2))
+    transparent = pygame.Surface((W, H))
+    transparent.set_alpha(10)
+
+    for i in range(3):
+        sc.blit(transparent, (0, 0))
+        sc.blit(paused_text, paused_rect)
+        pygame.display.flip()
+        pygame.time.wait(30)
+
+
+def get_balls(all_balls):
+    connection = sqlite3.connect('starry_rain1.sqlite')
+    cursor = connection.cursor()
+    connection.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS 'top' (id INTEGER, Username TEXT, Balls INTEGER)")
+    connection.commit()
+    result_id = cursor.execute("""SELECT id FROM top ORDER BY id DESC limit 1""").fetchall()
+    id1 = result_id[0][0]
+    cursor.execute("INSERT INTO top VALUES (?, ?,?)", (id1 + 1, '', all_balls))
+    connection.commit()
+    connection.close()
+
+
+def createBall(group, balls_surf, W):
+    indx = randint(0, len(balls_surf) - 1)
+    x = randint(20, W - 20)
+    speed = randint(1, 4)
+    return Ball(x, speed, balls_surf[indx], group)
+
+
+def createCoin(group, coins_surf, W):
+    # indx = randint(0, len(balls_surf) - 1)
+    x = randint(20, W - 20)
+    speed = randint(1, 4)
+    return Coin(x, speed, coins_surf, group)
+
+
+def collideBalls(balls, ship, boom_sound):
+    global game_score
+    global count_coins
+    global count_balls
+    global lives
+    global running
+    global all_balls
+    for ball in balls:
+        if ship.t_rect.collidepoint(ball.rect.center):
+            boom_sound.play()
+            ball.kill()
+            Particle.create_particles(ball.rect.center)
+            if lives > 1:
+                lives -= 1
+            else:
+                get_balls(all_balls)
+                running = False
+                all_balls = 0
+                lives = 3
+                count_coins = 0
+                count_balls = 0
+                from FINAL import finalScreen
+                finalScreen()
+            # проигрыш
+
+
+def collideCoins(coins, ship, coin_sound):
+    global count_coins
+    global count_balls
+    global game_score
+    global lives
+    for coin in coins:
+        if ship.t_rect.collidepoint(coin.rect.center):
+            # game_score += ball.score
+            coin_sound.play()
+            coin.kill()
+            count_coins += 5
+            count_balls += 10
+            if count_coins == 1:
+                count_coins = 0
+                if lives < 3:
+                    lives += 1
+
+
+
+class Particle(pygame.sprite.Sprite):
+    global sprites_sprites
+    fire = [load_image("star.png")]
+    # fire  = pygame.image.load('data/star.png').convert_alpha()
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy, group, W, H):
+        super().__init__(group)
+        global sprites_sprites
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+        self.velocity = [dx, dy]
+        self.rect.x, self.rect.y = pos
+        self.gravity = 1
+
+        self.screen_rect = (0, 0, W, H)
+
+    def update(self):
+        global screen_rect
+        self.velocity[1] += self.gravity
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        if not self.rect.colliderect(self.screen_rect):
+            self.kill()
+
+    def create_particles(position):
+        numbers = range(-50, 50)
+        for _ in range(20):
+            Particle(position, random.choice(numbers), random.choice(numbers),  sprites_sprites, 1300, 770)
+
+
+def game():
+    global count_coins
+    global count_balls
+    global sprites_sprites
+    global lives
+    global all_balls
+    pygame.init()
+    pygame.time.set_timer(pygame.USEREVENT, 1000)
+    BLACK = (0, 0, 0)
+    W, H = 1300, 770
+    sc = pygame.display.set_mode((W, H))
+    x = 10
+    y = 10
+    os.environ['Sp_VIDEO_WINDOW_POS'] = "%d,%d" % (x, y)
+    lives = 3
+    count_coins = 0
+    count_balls = 0
+    f = pygame.font.SysFont('arial', 30)
+    boom_sound = pygame.mixer.Sound('audio/boom_sound.wav')
+    coin_sound = pygame.mixer.Sound('audio/coin_sound.wav')
+    paused = False
+    bg = load_image("background.jpg")
+    ship = SpaceShip("data/ship.png", W, H)
+    clock = pygame.time.Clock()
+    fps = 60
+    balls_images = ['stone4.png', 'stone5.png', 'stone3.png']
+    balls_surf = [load_image(path) for path in balls_images]
+    coins_surf = load_image('coin.png')
+    balls = pygame.sprite.Group()
+    coins = pygame.sprite.Group()
+    createBall(balls, balls_surf, 1300)
+    createCoin(coins, coins_surf, 1300)
+    k = 0
+    speed = 10
+    running = True
+    all_sprites = pygame.sprite.Group()
+    sprites_sprites = pygame.sprite.Group()
+
+    heart1 = AnimatedLife(load_image('heart1.png'), 5, 2, 10, 70, all_sprites)
+    heart2 = AnimatedLife(load_image('heart1.png'), 5, 2, 70, 70, all_sprites)
+    heart3 = AnimatedLife(load_image('heart1.png'), 5, 2, 130, 70, all_sprites)
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.USEREVENT:
+                if not paused:
+                    createBall(balls, balls_surf, 1300)
+                    k += 1
+                    if not (k % 5):
+                        createCoin(coins, coins_surf, 1300)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    paused = not paused
+        if not paused:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                ship.t_rect.x -= speed
+                if ship.t_rect.x < 0:
+                    ship.t_rect.x = 0
+            elif keys[pygame.K_RIGHT]:
+                ship.t_rect.x += speed
+                if ship.t_rect.x > W - ship.t_rect.width:
+                    ship.t_rect.x = W - ship.t_rect.width
+
+            sc.blit(bg, (0, 0))
+            balls.draw(sc)
+            coins.draw(sc)
+
+            sc.blit(ship.image, ship.t_rect)
+            sc.blit(load_image('notch.png'), (0, 0))
+
+            all_balls = count_balls + k * 10
+            sc_str1 = f.render(str("Баллы"), 1, (0, 0, 0))
+            sc.blit(sc_str1, (20, 10))
+            sc_balls = f.render(str(all_balls), 1, (0, 0, 0))
+            sc.blit(sc_balls, (40, 40))
+            sc_str2 = f.render(str("Жизни"), 1, (0, 0, 0))
+            sc.blit(sc_str2, (120, 10))
+            sc_text = f.render(str(lives), 1, (0, 0, 0))
+            sc.blit(sc_text, (150, 40))
+
+            if lives == 2:
+                heart3.kill()
+            elif lives == 1:
+                heart2.kill()
+
+            all_sprites.draw(sc)
+            all_sprites.update()
+            pygame.display.update()
+            clock.tick(fps)
+            balls.update(H)
+            coins.update(H)
+            collideBalls(balls, ship, boom_sound)
+            collideCoins(coins, ship, coin_sound)
+        else:
+            pause(sc, 0, 0)
+
+game()
--- menu.py
+++ menu.py
@@ -1,109 +1,89 @@
 import sqlite3
 
 import pygame
+
 import pygame_menu
 from pygame_menu import themes
 
+def startScreen():
+    pygame.init()
+    surface = pygame.display.set_mode((600, 500), pygame.RESIZABLE)
+    pygame.display.set_caption("Starry rain")
+    start_menu = pygame_menu.Menu(
+        height=500,
+        theme=pygame_menu.themes.THEME_BLUE,
+        title='Welcome',
+        width=600
+    )
 
-# pygame.init()
-# surface = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
+    class Menu:
+        def __init__(self):
+            start_menu.add.label('STARRY RAIN')
+            start_menu.add.label(' ')
+            self.username = start_menu.add.text_input('Name: ', default=(f"user{self.get_id() + 1}"), maxchar=10)
+            start_menu.add.button('Hot Keys', self.hot_keys)
+            start_menu.add.selector('Difficulty :', [('Easy', 1), ('Normal', 2), ('Hard', 3)],
+                                    onchange=self.set_difficulty)
+            start_menu.add.button('Play', self.start_the_game)
+            start_menu.add.button('Quit', pygame_menu.events.EXIT)
+            start_menu.enable()
 
-class Menu():
-    def __init__(self):
-        # super().__init__()
-        pygame.init()
-        surface = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
-        pygame.display.set_caption("Starry rain")
-        self.start_menu = pygame_menu.Menu(
-            height=400,
-            theme=pygame_menu.themes.THEME_BLUE,
-            title='Welcome',
-            width=600
-        )
+            self.on_resize()
 
-        self.start_menu.add.label('STARRY RAIN')
-        self.start_menu.add.label(' ')
-        # start_menu.add.text_input('Name: ', default='user123', maxchar=10)
+            global hot_menu
+            hot_menu = pygame_menu.Menu('Hot keys', 600, 500, theme=themes.THEME_BLUE)
+            hot_menu.add.label(title='Esc - выход из игры')
+            hot_menu.add.label(title='Пробел - пауза')
 
-        self.username = self.start_menu.add.text_input('Name: ', default=('user' + self.get_id()), maxchar=10)
+            global loading
+            loading = pygame_menu.Menu('Loading the Game...', 600, 500, theme=themes.THEME_DARK)
+            loading.add.progress_bar("Progress", progressbar_id="1", default=0, width=200, )
 
-        self.start_menu.add.button('Hot Keys', self.hot_keys)
-        self.start_menu.add.button('Play', self.start_the_game)
-        self.start_menu.add.button('Quit', pygame_menu.events.EXIT)
-        self.start_menu.enable()
-        self.on_resize()
-        hot_menu = pygame_menu.Menu('Hot keys', 600, 400, theme=themes.THEME_BLUE)
-        hot_menu.add.label(title='Esc - выход из игры')
-        hot_menu.add.label(title='Пробел - пауза')
-        loading = pygame_menu.Menu('Loading the Game...', 600, 400, theme=themes.THEME_DARK)
-        loading.add.progress_bar("Progress", progressbar_id="1", default=0, width=200, )
-        arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size=(10, 15))
-        update_loading = pygame.USEREVENT + 0
+            global arrow
+            arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size=(10, 15))
 
-    def start_the_game(self):
-        self.start_menu._open(self.loading)
-        pygame.time.set_timer(self.update_loading, 30)
-        # name = username.get_value()  # имя
+        def start_the_game(self):
+            start_menu._open(loading)
+            pygame.time.set_timer(update_loading, 30)
 
-        self.name = self.username.get_value()
+            self.name = self.username.get_value()
 
-        connection = sqlite3.connect('starry_rain.sqlite')
-        cursor = connection.cursor()
-        result_id = cursor.execute("""SELECT id FROM top ORDER BY id DESC limit 1""").fetchall()
-        id = result_id[0][0]
-        cursor.execute("INSERT INTO top VALUES (?,?,?)", (id + 1, self.name, ''))
+            id1 = self.get_id()
 
-        connection.commit()
-        connection.close()
+            connection = sqlite3.connect('starry_rain1.sqlite')
+            cursor = connection.cursor()
 
-    def get_id(self):
-        connection = sqlite3.connect('starry_rain.sqlite')
-        cursor = connection.cursor()
-        result_id = cursor.execute("""SELECT id FROM top ORDER BY id DESC limit 1""").fetchall()
-        id = result_id[0][0]
+            cursor.execute("INSERT INTO top VALUES (?,?,?)", (id1 + 1, self.name, 0))
 
-        connection.commit()
-        connection.close()
-        return (id)
+            connection.commit()
+            connection.close()
 
-    def hot_keys(self):
-        self.start_menu._open(self.hot_menu)
+        def set_difficulty(self, value, difficulty):
+            print(value)
+            print(difficulty)
 
-    def on_resize(self):
-        window_size = surface.get_size()
-        new_w, new_h = window_size[0], window_size[1]
-        self.start_menu.resize(new_w, new_h)
+        def hot_keys(self):
+            start_menu._open(hot_menu)
 
+        def on_resize(self):
+            window_size = surface.get_size()
+            new_w, new_h = window_size[0], window_size[1]
+            start_menu.resize(new_w, new_h)
 
+        def get_id(self):
+            connection = sqlite3.connect('starry_rain1.sqlite')
+            cursor = connection.cursor()
+            connection.commit()
+            cursor.execute("CREATE TABLE IF NOT EXISTS 'top' (id INTEGER, Username TEXT, Balls INTEGER)")
+            connection.commit()
+            result_id = cursor.execute("""SELECT id FROM top ORDER BY id DESC limit 1""").fetchall()
+            id1 = result_id[0][0]
+            return (id1)
 
-# pygame.display.set_caption("Starry rain")
-# start_menu = pygame_menu.Menu(
-#     height=400,
-#     theme=pygame_menu.themes.THEME_BLUE,
-#     title='Welcome',
-#     width=600
-# )
-#
-# start_menu.add.label('STARRY RAIN')
-# start_menu.add.label(' ')
-# # start_menu.add.text_input('Name: ', default='user123', maxchar=10)
-#
-# username = start_menu.add.text_input('Name: ', default=('user' + get_id()), maxchar=10)
-#
-# start_menu.add.button('Hot Keys', hot_keys)
-# start_menu.add.button('Play', start_the_game)
-# start_menu.add.button('Quit', pygame_menu.events.EXIT)
-# start_menu.enable()
-# on_resize()
-# hot_menu = pygame_menu.Menu('Hot keys', 600, 400, theme=themes.THEME_BLUE)
-# hot_menu.add.label(title='Esc - выход из игры')
-# hot_menu.add.label(title='Пробел - пауза')
-# loading = pygame_menu.Menu('Loading the Game...', 600, 400, theme=themes.THEME_DARK)
-# loading.add.progress_bar("Progress", progressbar_id="1", default=0, width=200, )
-# arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size=(10, 15))
-# update_loading = pygame.USEREVENT + 0
-fl = True
-if __name__ == '__main__':
+    fl = True
+    update_loading = pygame.USEREVENT + 0
+    Menu()
+    # start_menu.mainloop(surface)
     while fl:
         events = pygame.event.get()
         for event in events:
@@ -112,20 +92,42 @@ if __name__ == '__main__':
                 progress.set_value(progress.get_value() + 1)
                 if progress.get_value() == 100:
                     pygame.time.set_timer(update_loading, 0)
+
+                    connection = sqlite3.connect('starry_rain1.sqlite')
+                    cursor = connection.cursor()
+
+                    prev_id = cursor.execute("""SELECT id FROM LastState ORDER BY id DESC limit 1""").fetchall()
+                    id1 = prev_id[0][0]
+                    cursor.execute("INSERT INTO LastState (id, State) VALUES (?, ?)", (id1 + 1, 'GAME'))
+
+                    connection.commit()
+                    connection.close()
+                    fl = False
+                    from new import game
+                    game()
             if event.type == pygame.QUIT:
+                connection = sqlite3.connect('starry_rain1.sqlite')
+                cursor = connection.cursor()
+
+                prev_id = cursor.execute("""SELECT id FROM LastState ORDER BY id DESC limit 1""").fetchall()
+                id1 = prev_id[0][0]
+                cursor.execute("INSERT INTO LastState (id, State) VALUES (?, ?)", (id1 + 1, 'EXIT'))
+
+                connection.commit()
+                connection.close()
                 fl = False
-                # pygame.quit()
-                # break
-            if event.type == pygame.VIDEORESIZE:
-                surface = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-                self.on_resize()
-        if start_menu.is_enabled():
-            start_menu.update(events)
-            start_menu.draw(surface)
-            if start_menu.get_current().get_selected_widget():
-                arrow.draw(surface, start_menu.get_current().get_selected_widget())
+            # if event.type == pygame.VIDEORESIZE:
+            #     surface = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+            #     Menu.on_resize(Menu)
+        # if start_menu.is_enabled():
+        #     start_menu.update(events)
+        #     start_menu.draw(surface)
+        #     if start_menu.get_current().get_selected_widget():
+        #         arrow.draw(surface, start_menu.get_current().get_selected_widget())
         surface.fill((25, 0, 50))
         start_menu.update(events)
         start_menu.draw(surface)
-
         pygame.display.flip()
+
+
+startScreen()
